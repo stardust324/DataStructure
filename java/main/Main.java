93429ea364104ddafa5607b7cacd092970bce6d5
stardust324 <148509236+stardust324@users.noreply.github.com>
2024-09-08 19:38:15 +0800

    初始化项目

---
+++ java/main/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @Description
+ * @Author 尘
+ * @Date 2024/9/8 19:32
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
