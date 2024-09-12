250e21e9c12e7fb8b05f029688125f310267bf24
stardust324 <148509236+stardust324@users.noreply.github.com>
2024-09-12 15:01:00 +0800

    共享栈，暂未实现

---
+++ java/main/Stack/ShareStack.java
@@ -0,0 +1,9 @@
+package Stack;
+
+/**
+ * @Description
+ * @Author 尘
+ * @Date 2024/9/12 14:59
+ */
+public interface ShareStack <T>{
+}
---
+++ java/main/Stack/ShareStackiml.java
@@ -0,0 +1,9 @@
+package Stack;
+
+/**
+ * @Description
+ * @Author 尘
+ * @Date 2024/9/12 15:00
+ */
+public class ShareStackiml <T> implements ShareStack<T>{
+}
